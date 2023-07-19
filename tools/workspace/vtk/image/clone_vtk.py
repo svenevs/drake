950baec8bd8b0f9883f6f684de52483cab48e777
Stephen McDowell <stephen.mcdowell@kitware.com>
2023-07-19 15:44:34 -0400

    --filter=blob:none

--- tools/workspace/vtk/image/clone_vtk.py
+++ tools/workspace/vtk/image/clone_vtk.py
@@ -16,13 +16,13 @@ def clone_vtk(vtk_git_ref: str, dest_dir: Path):
     """
     Clone VTK to dest_dir and checkout the specified vtk_git_ref.
     """
-    # NOTE: a full clone is required, VTK_GIT_REF can be a commit, branch, tag,
-    # or anything between.  Since cloning VTK can take a while, display the
-    # progress meter.
+    # NOTE: vtk_git_ref can be a commit, branch, tag, or anything between.
+    # Since cloning VTK can take a while, display the progress meter.
     subprocess.check_call(
         [
             "git",
             "clone",
+            "--filter=blob:none",
             "--progress",
             "--verbose",
             "https://gitlab.kitware.com/vtk/vtk.git",
